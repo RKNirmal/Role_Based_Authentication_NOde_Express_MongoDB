087a4645ee9d527129e6fae728ee3bcf7bc84a6c
RKNirmal <rnirmalkumar1794@gmail.com>
2021-10-07 09:58:37 +0530

    Add files via upload

--- models/admin.js
+++ models/admin.js
@@ -1,41 +1,55 @@
+const jwt = require('jsonwebtoken')
 //initialize environmental variables
 require('dotenv/config')
-    //initialize user schema
+//initialize user schema
 var userSchema = require('./user.js')
-    //initialize the encryption decryption function from cypher.js
+//initialize the encryption decryption function from cypher.js
 const enc = require('../enc/cypher')
-    //storing admin name from environmental variable
+//storing admin name from environmental variable
 var candidateName = process.env.NAMEADMIN
 var findName = {}
 findName.username = candidateName
 
 //finding a particular user in the database to check for the user already existing or not
-userSchema.findOne(findName, function(err, user) {
-    if (err) {
-        console.log(err)
-    } else if (user) {
-        console.log('Admin user already exists!')
-    } else {
-        //user details of admin initialized in this variable
-        var userDetails = {
-                firstName: 'admin',
-                lastName: 'user',
-                email: 'admin@admin.com',
-                address: 'address',
-                city: 'city',
-                state: 'state',
-                country: 'country',
-                zip: '3000',
-                username: process.env.NAMEADMIN,
-                password: enc.encrypt(process.env.PASSADMIN),
-                role: 'admin'
-            }
-            //creation of admin user with the above initialized admin details from userdetails variable
-        userSchema.create(userDetails, function(e) {
-            if (e) {
-                throw e
-            }
-        })
-        console.log('Admin account created successfully')
+userSchema.findOne(findName, function (err, user) {
+  if (err) {
+    console.log(err)
+  } else if (user) {
+    console.log('Admin user already exists!')
+  } else {
+    //user details of admin initialized in this variable
+    var userDetails = {
+      firstName: 'admin',
+      lastName: 'user',
+      email: 'admin@admin.com',
+      address: 'address',
+      city: 'city',
+      state: 'state',
+      country: 'country',
+      zip: '3000',
+      username: process.env.NAMEADMIN,
+      password: enc.encrypt(process.env.PASSADMIN),
+      role: 'admin',
+      token: String
     }
+    const token = jwt.sign(
+      {
+        username: userDetails.username,
+        password: userDetails.password
+      },
+      process.env.PASSWD
+    )
+    userDetails.token = token
+    process.env.adminToken = token
+    console.log(process.env.adminToken)
+
+    //creation of admin user with the above initialized admin details from userdetails variable
+    userSchema.create(userDetails, function (e) {
+      if (e) {
+        throw e
+      }
+      console.log('Admin user signed with token: ' + userDetails.token)
+    })
+    console.log('Admin account created successfully')
+  }
 })
--- models/product.js
+++ models/product.js
@@ -3,7 +3,19 @@ var productSchema = new mongoose.Schema({
   productName: String,
   productDesc: String,
   productPrice: Number,
-  productCategory: String
+  productCategory: String,
+  productImg: {
+    type: Buffer,
+    required: true
+  }
 })
 
-module.exports = mongoose.model('Product', productSchema, 'products')
+// a virtual is a property that is not stored in MongoDB. Virtuals are typically used for computed properties on documents.
+// IT WILL GIVE US OUR IMAGE SOURCE THAT WE WILL USE IN OUT IMG TAG
+productSchema.virtual('coverImage').get(function () {
+  if (this.productImg != null) {
+    return `data:charset=utf-8;base64,${this.productImg.toString('base64')}`
+  }
+})
+
+module.exports = mongoose.model('product', productSchema, 'products')
